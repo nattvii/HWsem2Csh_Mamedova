b5249beb856960b5cdef2482fc7da4a0518d6d3c
nattvii <natt.mmd@gmail.com>
2023-01-08 20:21:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.Write("Введи трёхзначное число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(n2);
+Console.WriteLine("Вторая цифра числа = "+ stringNumber[1]);
